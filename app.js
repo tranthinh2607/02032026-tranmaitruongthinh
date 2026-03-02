9fec7053492d6c7e10f5d0420beae40297249818
tranthinh2607 <tranmaitruongthinh2607@gmail.com>
2026-03-02 13:53:27 +0700

    fix bugg

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-mongoose.connect('mongodb://localhost:27017');
+mongoose.connect('mongodb://127.0.0.1:27017');
 mongoose.connection.on('connected', () => {
   console.log("connected");
 })
